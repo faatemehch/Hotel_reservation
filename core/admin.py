208e37a63eb0834efc0bd9b06b026465fbd3ec0a
Fatemeh Chamani <fch199674@gmail.com>
2024-03-16 16:24:11 +0330

    user admin updated

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,8 @@ from .models import CustomUser
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'is_staff', 'is_active')
+    list_per_page = 10
+    search_fields = ('username', 'email')
+    list_display_links = ('id','username')
 
 admin.site.register(CustomUser, CustomUserAdmin)
