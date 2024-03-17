eb8e6ea307758170cbb98934b662ab287484e5cb
Fatemeh Chamani <fch199674@yahoo.com>
2024-03-17 08:10:52 +0330

    Hotel Room Reservation Review models and admins added

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Hotel, Room, Review, Reservation
 
-# Register your models here.
+
+class RoomInline(admin.TabularInline):
+    model = Room
+    fields = ('room_type', 'base_price', 'max_occupancy')
+    extra = 1
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'is_active')
+    inlines = (RoomInline, )
+    search_fields = ('name',)
+    prepopulated_fields = {
+        'slug': ("name",),
+    }
+    list_per_page = 10
+    search_fields = ('name',)
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('__str__','hotel', 'base_price')
+    list_filter = ('base_price',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'writer', 'is_active')
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status', 'check_in_date',
+                    'check_out_date', 'guests')
--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+def get_file_name_ext(file_path):
+    base_name = os.path.basename(file_path)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, fileName):
+    _, ext = get_file_name_ext(fileName)
+    final_name = f'{instance.id}-{instance.name}.{ext}'
+    return f'hotels/{final_name}'
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to=upload_image_path)
+    phone_number = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=255)
+    description = models.TextField()
+    additional_info = models.TextField()
+    star_rating = models.DecimalField(decimal_places=1, max_digits=2)
+    base_price = models.DecimalField(decimal_places=2, max_digits=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modiefied_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("hotel", kwargs={"pk": self.slug})
+
+
+class Room(models.Model):
+    ROOM_TYPES = (
+        ('s', 'Single'),
+        ('d', 'Double'),
+        ('st', 'Suit'),
+    )
+    hotel = models.ForeignKey(Hotel, on_delete=models.SET_NULL, null=True)
+    room_type = models.CharField(max_length=10, default=ROOM_TYPES[0][0])
+    base_price = models.DecimalField(decimal_places=2, max_digits=4)
+    max_occupancy = models.PositiveSmallIntegerField()  # number of guests allowed
+    created_at = models.DateTimeField(auto_now_add=True)
+    modiefied_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.room_type
+
+class Review(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.SET_NULL, null=True)
+    writer = models.CharField(max_length=255)
+    body = models.TextField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    customer_rate = models.PositiveSmallIntegerField()
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modiefied_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.hotel
+    
+
+
+
+class Reservation(models.Model):
+    RESERVATION_STATUS = (
+        ('p', 'Paid'),
+        ('u', 'Un paid'),
+        ('c', 'cancelled'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.SET_NULL, null=True)
+    # hotel = models.ForeignKey(Hotel, on_delete=models.SET_NULL)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(
+        max_length=1, choices=RESERVATION_STATUS, default=RESERVATION_STATUS[1][0])
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+    guests = models.PositiveSmallIntegerField()  # starts from  0 to 32767
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modiefied_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} | {self.room.hotel.name}"
