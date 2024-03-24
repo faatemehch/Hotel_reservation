09feeb2ac4d36b27989155a77992fec71f70435e
Fatemeh Chamani <fch199674@yahoo.com>
2024-03-24 11:33:55 +0330

    home view and home url added

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('hotel/', include('hotel.urls')),
     path('accounts/', include('allauth.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
