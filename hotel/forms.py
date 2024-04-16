128ce1813b52a4cba655e9d69030c4cd0fc2e4ce
Fatemeh Chamani <fch199674@yahoo.com>
2024-04-16 09:44:33 +0330

    fix bugs in hotel app

--- hotel/forms.py
+++ hotel/forms.py
@@ -6,5 +6,8 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ('writer', 'body', 'customer_rate')
         widgets = {
-            'customer_rate' : forms.NumberInput(attrs={'min':1, 'max':5})
+            'customer_rate' : forms.NumberInput(attrs={'min':1, 'max':5, 'placeholder':'Rating'}),
+            'writer' : forms.TextInput(attrs={'placeholder':'Your Fullname'}),
+            'body' : forms.Textarea(attrs={'placeholder' : 'Your review'}),
         }
+
